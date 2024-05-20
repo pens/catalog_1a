f2ff1119ef36fdb8f6293ad49ab691ea3b791fec
Seth Pendergrass <8273706+pens@users.noreply.github.com>
2024-05-19 21:54:41 -0700

    Ready for test run?

--- exiftool/run.sh
+++ exiftool/run.sh
@@ -5,16 +5,16 @@ cp test_image.jpg b.jpg
 
 # -tagsFromFiles.
 
-exiftool -q -overwrite_original -Artist="Alice" a.jpg
+exiftool -q -overwrite_original -Artist="Alice" -Copyright="Owner" a.jpg
 exiftool -q -overwrite_original -Artist="Bob" b.jpg
 
 # Only copies specified tags.
 exiftool -q -overwrite_original -tagsFromFile a.jpg -Copyright b.jpg
-exiftool -Artist b.jpg
+exiftool -T -FileName -Artist b.jpg
 
 # Copies all tags.
 exiftool -q -overwrite_original -tagsFromFile a.jpg b.jpg
-exiftool -Artist b.jpg
+exiftool -T -FileName -Artist b.jpg
 
 # -tagsFromFiles for rename.
 
@@ -25,7 +25,7 @@ exiftool -q -overwrite_original -DateTimeOriginal="2024:5:18 5:03:00" b.jpg
 
 # Need to specify DateTimeOriginal tag or this will copy *all*.
 exiftool -q -overwrite_original -tagsFromFile a.jpg -DateTimeOriginal -d "%y%m%d_%H%M%S" b.jpg
-exiftool -Artist -DateTimeOriginal b.jpg
+exiftool -T -FileName -Artist -DateTimeOriginal b.jpg
 
 # Renaming.
 
@@ -36,10 +36,21 @@ exiftool -q -overwrite_original -DateTimeOriginal="2024:5:18 5:03:00" b.jpg
 
 # If redirecting with <, only specified tags copied.
 exiftool -q -overwrite_original -tagsFromFile a.jpg -d "%y%m%d_%H%M%S" "-TestName<DateTimeOriginal" b.jpg
-exiftool -Artist b.jpg
+exiftool -T -FileName -Artist b.jpg
 
 # Get rename output, which can be compared to FileName.
 exiftool -q -overwrite_original -tagsFromFile a.jpg -d "%y%m%d_%H%M%S" "-TestName<DateTimeOriginal" b.jpg
-exiftool -FileName b.jpg
+exiftool -T -FileName b.jpg
 
-rm a.jpg b.jpg
+# Creating XMPs.
+
+exiftool -q -overwrite_original -Artist="Alice" a.jpg
+exiftool -q -overwrite_original -Artist="Bob" b.jpg
+exiftool -q -overwrite_original -DateTimeOriginal="1970:1:1 12:00:00" a.jpg
+exiftool -q -overwrite_original -DateTimeOriginal="2024:5:18 5:03:00" b.jpg
+
+exiftool -o a.xmp a.jpg
+exiftool -T -FileName -Artist -DateTimeOriginal a.jpg
+exiftool -T -FileName -Artist -DateTimeOriginal a.xmp
+
+rm a.jpg b.jpg a.xmp
